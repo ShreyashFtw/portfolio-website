82d9f136452045229193b1a21b13dd51635f37c7
Shreyash <shreyashthaware3@gmail.com>
2024-01-01 01:50:19 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/0Q54NZnP/todo.jpg',
+    title: " To-do-List",
+    desc: "",
+    link: "",
+    git: "https://github.com/ShreyashFtw/To-do-list.git",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/kGdtcttY/rohitsharma-cropped-fg3ec7qrkgv1b6j0scq9o1gr.jpg',
+    title: "Rohit Sharma ODI decade review",
+    link: "https://github.com/ShreyashFtw/Rohit-Sharmas-Decade-in-Review-ODI-Stats-2007-2019/blob/main/RohitSharmaodi2007_2019.ipynb",
+    git: "https://github.com/ShreyashFtw/Rohit-Sharmas-Decade-in-Review-ODI-Stats-2007-2019.git",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/HLJvyfmW/bg.jpg',
+    title: "Netflix clone ",
+    desc: "",
+    link: "https://shreyash-netflix-clone.netlify.app/",
+    git: "https://github.com/ShreyashFtw/netflix-clone.git",
+  }
+  
+];
